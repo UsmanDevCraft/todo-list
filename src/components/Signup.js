09da450078feaf6a589d6704cd67965d1e0ce535
Usman Awan <uawan8154@gmail.com>
2024-07-31 14:24:48 +0100

    converted postman api to vercel api.

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -13,7 +13,7 @@ const Signup = () => {
 
     const fetchUser = async (e) => {
         e.preventDefault()
-        const response = await fetch("http://localhost:5000/api/auth/createuser", {
+        const response = await fetch("https://backend-todolist-tau.vercel.app/api/auth/createuser", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
